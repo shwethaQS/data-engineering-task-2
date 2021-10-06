017a721af97d36c358efb394e50283dfc07d6d90
paddy.alton <paddy.alton@apolitical.co>
2021-10-06 14:41:48 +0100

    Infrastructure created

---
+++ src/main.py
@@ -0,0 +1,40 @@
+# main.py
+
+from pyspark.sql import SparkSession
+from sys import path
+from time import time
+
+path.insert(0, "/app")
+
+
+def myjob(spark: SparkSession, **kwargs):
+    """
+    myjob
+
+    Main entrypoint for PySpark job
+
+    INPUTS:
+        spark - active SparkSession
+        **kwargs - keyword arguments from spark-submit
+
+    """
+    df = spark.read.csv("spark-data/Medals.csv")
+
+    df.show()
+
+
+if __name__ == "__main__":
+
+    spark = (
+        SparkSession
+            .builder
+            .appName("exercise")
+            .master("spark://spark-master:7077")
+            .getOrCreate()
+    )
+
+    start = time()
+    myjob(spark)
+    end = time()
+
+    print(f"\nExecution of job took {end-start:.8f} seconds\n")
